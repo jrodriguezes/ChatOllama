55c7bff064293e93bd01f5eea154d37999df5afd
jerem <jerem@Jeremy>
2024-12-29 23:15:07 -0600

    Interfaz base para IA

---
+++ src/main/java/com/mycompany/chatia/main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.chatia;
+
+/**
+ *
+ * @author jerem
+ */
+public class main {
+    
+    public static void main(String[] args) {
+        Chat Pr = new Chat();
+        Pr.setVisible(true);
+        Pr.setLocationRelativeTo(null);
+    }
+}
